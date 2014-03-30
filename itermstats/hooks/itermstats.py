6dc842e2fe8851601537e8b0df33b7a24436ff27
Torsten Rehn <torsten@rehn.email>
2014-03-30 09:32:38 +0200

    add itermstats plugin

---
+++ itermstats/hooks/itermstats.py
@@ -0,0 +1,43 @@
+from base64 import b64encode
+from os import environ, remove
+from tempfile import NamedTemporaryFile
+
+import cairosvg
+from pygal import Config, Pie
+from pygal.style import Style
+
+
+STYLE = Style(
+    background='transparent',
+    opacity=1,
+    plot_background='transparent',
+    colors=('#00ae19', '#25ff44', '#ffde00', '#c90000'),
+)
+
+def node_apply_end(repo, node, duration=None, interactive=None, result=None, **kwargs):
+    if environ.get('TERM_PROGRAM', None) != "iTerm.app" or not interactive:
+        return
+
+    css_file = NamedTemporaryFile(delete=False)
+    css_file.write(".text-overlay { display: none; }")
+    css_file.close()
+
+    config = Config(
+        height=150,
+        style=STYLE,
+        width=350,
+    )
+    config.css.append(css_file.name)
+
+    chart = Pie(config)
+    chart.add('correct', result.correct)
+    chart.add('fixed', result.fixed)
+    chart.add('skipped', result.skipped)
+    chart.add('failed', result.failed)
+
+    png_data = cairosvg.svg2png(bytestring=chart.render())
+    png_data_b64 = b64encode(png_data)
+
+    remove(css_file.name)
+
+    print("\033]1337;File=inline=1:{}\007".format(png_data_b64))
