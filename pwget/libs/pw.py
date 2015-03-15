7f6af20b8ef89b228723c88789d4c672be5361fb
Torsten Rehn <torsten@rehn.email>
2015-03-15 12:56:28 +0100

    pwget: add Keychain support

--- pwget/libs/pw.py
+++ pwget/libs/pw.py
@@ -2,7 +2,10 @@ import hashlib
 import hmac
 from os.path import join
 from random import Random
+from pipes import quote
 from string import ascii_letters, punctuation, digits
+from subprocess import CalledProcessError, check_output
+from sys import platform
 
 
 SECRET_FILENAME = "pwget.secret"
@@ -14,12 +17,41 @@ def ensure_secret(path):
         with open(secret_path) as f:
             secret = f.read()
     except IOError:
+        if platform == 'darwin':
+            try:
+                return check_output([
+                    "security",
+                    "find-generic-password",
+                    "-a",
+                    "bw_pwget_" + path,
+                    "-s",
+                    "bundlewrap",
+                    "-w",
+                ]).strip()
+            except CalledProcessError:
+                raise IOError(
+                    "Unable to read pwget secret from {path} or Mac OS Keychain. "
+                    "You can create a new one with:\n\n"
+                    "# dd if=/dev/urandom bs=64 count=1 | base64 > pwget.secret\n\n"
+                    "or (better)\n\n"
+                    "# pwgetsecret=`dd if=/dev/urandom bs=64 count=1 | base64`; "
+                    "security add-generic-password -a {account} "
+                    "-w $pwgetsecret -s bundlewrap\n\n"
+                    "or if your team has already created a secret, paste it into this:\n\n"
+                    "# read pwgetsecret; "
+                    "security add-generic-password -a {account} "
+                    "-w $pwgetsecret -s bundlewrap".format(
+                        account=quote("bw_pwget_" + path),
+                        path=secret_path,
+                    ),
+                )
         raise IOError(
             "Unable to read pwget secret from {path}. "
             "You can create a new one with:\n\n"
             "# dd if=/dev/urandom bs=64 count=1 | base64 > pwget.secret".format(
                 path=secret_path,
-        ))
+            ),
+        )
     return secret
 
 
